3b476e4dede296e9684fc41d4e8a9f07a2815dd4
Imran Ali <imranrali1@gmail.com>
2020-09-29 19:53:05 -0400

    learning vars and assignment

--- src/github.com/imran-kkz/goNow/main.go
+++ src/github.com/imran-kkz/goNow/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+var i int = 42
+
+var (
+	actorName    string = "imran"
+	seasonNumber int    = 3
+)
+
 func main() {
-	fmt.Println("Hello, World!")
+	i := 42
+	fmt.Printf("%v, %T", i, i)
 }
